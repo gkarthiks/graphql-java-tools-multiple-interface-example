9538aca228397751cd96c490fde215ffad7663ad
Karthikeyan Govindaraj <30545166+gkarthics@users.noreply.github.com>
2017-08-07 13:24:35 -0700

    Adding mutations

--- src/main/java/com/graphql/SecondRepo.java
+++ src/main/java/com/graphql/SecondRepo.java
@@ -9,14 +9,17 @@ public class SecondRepo {
 
 	    public SecondRepo() {
 	    	secondImplValLst = new ArrayList<>();
-	        //add some links to start off with
-	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_01", "2ndImplIdVal_01", "2ndImplValue_01"));
-	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_02", "2ndImplIdVal_02", "2ndImplValue_02"));
-	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_03", "2ndImplIdVal_03", "2ndImplValue_03"));
+
+	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_01", "2ndImplIdVal_01", "2ndImplValue_01", "1stInterfaceVal_01"));
+	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_02", "2ndImplIdVal_02", "2ndImplValue_02", "1stInterfaceVal_01"));
+	    	secondImplValLst.add(new SecondImpl("2ndInterfaceVal_03", "2ndImplIdVal_03", "2ndImplValue_03", "1stInterfaceVal_01"));
 	    }
 
 	    public List<SecondImpl> getSecondImplValues() {
 	        return secondImplValLst;
 	    }
 
+	    public void saveSecondImpl(SecondImpl secondImpl) {
+	    	secondImplValLst.add(secondImpl);
+	    }
 }
